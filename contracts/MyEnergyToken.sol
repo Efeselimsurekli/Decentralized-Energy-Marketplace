c99e09e8bb6e3f66a8457d98cac815922c330583
Efeselimsurekli <147747741+Efeselimsurekli@users.noreply.github.com>
2025-11-26 20:57:15 +0300

    Create MyEnergyToken contract with mint and burn functions

---
+++ contracts/MyEnergyToken.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.18; // Sürümü sabitledik
+
+import "@openzeppelin/contracts@4.9.3/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts@4.9.3/access/Ownable.sol";
+
+contract MyEnergyToken is ERC20, Ownable {
+    constructor(uint256 initialSupply) ERC20("EnergyToken", "ENG") {
+        _mint(msg.sender, initialSupply * 10 ** decimals());
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+
+    function burn(uint256 amount) public {
+        _burn(msg.sender, amount);
+    }
+}
